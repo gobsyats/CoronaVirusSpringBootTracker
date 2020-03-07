b025d21793129ff7e8c1fefce48e932bade9cfb0
Govind Yatnalkar <43868701+gobsyats@users.noreply.github.com>
2020-03-07 10:52:38 -0800

    First Commit for Corona Tracker

---
+++ src/main/java/com/gobsyats/CorTrack/CorTrackApplication.java
@@ -0,0 +1,15 @@
+package com.gobsyats.CorTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CorTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gobsyats/CorTrack/CorTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gobsyats.CorTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
